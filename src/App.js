980c9bcf098e0a9a4a5c40ac94f24e14e08c0165
Roberto <rrh2023@bu.edu>
2025-01-24 09:33:24 -0500

    1/23

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import {useState} from 'react'
 import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem"
+import FeedbackList from "./components/FeedbackList"
 import FeedbackData from './data/FeedbackData'
 
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData)
-
+    const deleteFeedback = id => {
+        console.log('App', id)
+    }
     return  (
         <>
         <Header bgColor='rgb(0,0,0,0.4)' textColor='#ff6a95' text="Feedback UI"/>
         <div className='container'>
-            <FeedbackItem/>
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>  
         </>
-        
     )
 }
 
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import {useState} from 'react'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+import {FaTimes} from 'react-icons/fa'
 
-function FeedbackItem() {
+function FeedbackItem({item, handleDelete}) {
   const [rating, setRating] = useState(7)
   const [text, setText] = useState('This is an example of a feedback item')
 
+  
   return (
-    <div className='card'>
-        <div className="num-display">{rating}</div>
-        <div className="text-display">{text}</div>
-    </div>
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close">
+            <FaTimes color='purple'/>
+        </button>
+        <div className="text-display">{item.text}</div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
