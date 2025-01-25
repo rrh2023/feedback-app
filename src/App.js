cbd5e8505169392c3900a5b06414218f5880a50e
Roberto <rrh2023@bu.edu>
2025-01-25 06:49:01 -0500

    1/24

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import {useState} from 'react'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData)
     const deleteFeedback = id => {
-        console.log('App', id)
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
     }
     return  (
         <>
         <Header bgColor='rgb(0,0,0,0.4)' textColor='#ff6a95' text="Feedback UI"/>
         <div className='container'>
+            <FeedbackForm/>
+            <FeedbackStats feedback={feedback}/>
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>  
         </>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {useState} from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('Hello')
+
+  const handleTextChange = e => {
+    if(text === ''){
+        setBtnDisabled(true)
+        setMessage(null)
+    }
+    setText(e.target.value)
+  }
+
+  return (
+    <Card>
+        <form>
+            <h2>How would you rate your service with us?</h2>
+
+            <div className="input-group">
+                <input 
+                onChange={handleTextChange} 
+                type="text" 
+                placeholder='Write a review' 
+                value={text}/>
+                <Button type="submit" version="primary" isDisabled={btnDisabled}>Send</Button>
+            </div>
+
+            {message && <div className='message'>{message}</div>}
+        </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
