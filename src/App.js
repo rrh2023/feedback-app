78f9fd5a5aa64e46e205dd414bccdf2dbf89f678
Roberto <rrh2023@bu.edu>
2025-01-30 06:36:33 -0500

    1/30

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import {v4 as uuidv4} from 'uuid'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import {useState} from 'react'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutIconLink from './components/AboutIconLink'
@@ -11,12 +8,6 @@ import AboutPage from './pages/AboutPage'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
-
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback,...feedback])
-    }
 
    
     return  (
@@ -27,7 +18,7 @@ function App(){
                 <Routes>
                     <Route exact path='/' element={
                         <>
-                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackForm/>
                             <FeedbackStats/>
                             <FeedbackList />
                         </>
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import {useState, useContext} from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
-import {FaTimes} from 'react-icons/fa'
+import {FaTimes, FaEdit} from 'react-icons/fa'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackItem({item}) {
+  const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
+
+
   const [rating, setRating] = useState(7)
   const [text, setText] = useState('This is an example of a feedback item')
 
@@ -12,8 +16,11 @@ function FeedbackItem({item}) {
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
-        <button onClick={() => handleDelete(item.id)} className="close">
+        <button onClick={() => deleteFeedback(item.id)} className="close">
             <FaTimes color='purple'/>
+        </button>
+        <button onClick={() => editFeedback(item)} className="edit">
+          <FaEdit color='purple' />
         </button>
         <div className="text-display">{item.text}</div>
     </Card>
